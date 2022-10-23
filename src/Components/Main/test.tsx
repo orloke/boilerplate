2875ba1cbff5ab077ab60deaf14757cb5c95e94e
Junior Dering <juniordering@hotmail.com>
2022-10-23 10:55:57 -0400

    improving tests

--- src/Components/Main/test.tsx
+++ src/Components/Main/test.tsx
@@ -8,4 +8,10 @@ describe('<Main/>', () => {
       screen.getByRole('heading', { name: /react avançado/i }),
     ).toBeInTheDocument();
   });
+  it('should render the color correctly', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+    });
+  });
 });
