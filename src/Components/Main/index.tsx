dbd50bb585131116647171cb3ec3a98fc8f80280
Junior Dering <juniordering@hotmail.com>
2022-12-02 13:18:50 -0400

    building global styles

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,16 +1,8 @@
 import * as S from './styles';
 
 export const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="img/logo.svg"
-      alt="Imagem de um atomo com React Avançado escrito ao lado"
-    />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>TypeScript, React, NextJS e Styled Components</S.Description>
-    <S.Ilustration
-      src="img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente de um tela com código"
-    />
-  </S.Wrapper>
+  <div>
+    <h1>Nextjs</h1>
+    <a href="#">Link</a>
+  </div>
 );
